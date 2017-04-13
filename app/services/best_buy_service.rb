47dcb1119fe5696122fe67260d2393cefea1a2c1
Jonathan Serrano <jonathan.serranor@gmail.com>
2017-04-13 09:45:58 -0600

    adds correct best buy endpoint
    fills out the rest of the find by zip method, still testing reply

--- app/services/best_buy_service.rb
+++ app/services/best_buy_service.rb
@@ -1,7 +1,10 @@
 class BestBuyService
 
-  def find_by_zip(zip)
+  def self.find_by_zip(zip)
     response = 
-    Faraday.get("")
+    Faraday.get("https://api.bestbuy.com/v1/stores(area(#{zip},25))?format=json&show=storeId,storeType,name,city,region&apiKey=#{ENV['BEST_BUY_KEY']}")
+    final = JSON.parse(response.body, symbolize_names: true)
+    byebug
+    final
   end
 end
