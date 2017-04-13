ad434f830df7c638ed9bc739b72817d93902128f
Jonathan Serrano <jonathan.serranor@gmail.com>
2017-04-13 10:11:02 -0600

    best buy poro test is passing therefore bestbuy poro is able to save all required data, name, city etc and be used accordingly

--- app/models/best_buy.rb
+++ app/models/best_buy.rb
@@ -1,17 +1,22 @@
 class BestBuy
-  attr_reader :name
-              :city
-              :distance
-              :phone
+  attr_reader :name,
+              :city,
+              :distance,
+              :phone,
               :type
 
   def initialize(store_information)
-    @name = store_information[:name]
+    @name = store_information[:longName]
     @city = store_information[:city]
     @distance = store_information[:distance]
     @phone = store_information[:phone]
-    @type = store_information[:type]
+    @type = store_information[:storeType]
   end
 
-
+  def self.find_by_zip(zip)
+    raw_data = BestBuyService.find_by_zip(zip)
+    raw_data.map do |store_information|
+      BestBuy.new(store_information)
+    end
+  end
 end
--- spec/models/best_buy_spec.rb
+++ spec/models/best_buy_spec.rb
@@ -3,17 +3,17 @@ require 'rails_helper'
 describe BestBuy do
   context "find by zip" do
     it "returns an array bestbuy store objects" do
-      stores = BestBuyService.find_by_zip("80202")
+      stores = BestBuy.find_by_zip("80202")
       store = stores[0]
 
       expect(stores).to be_a(Array)
       expect(stores.count).to eq(10)
 
-      expect(bestbuy.name).to be_truthy
-      expect(bestbuy.city).to be_truthy
-      expect(bestbuy.distance).to be_truthy
-      expect(bestbuy.phone).to be_truthy
-      expect(bestbuy.type).to be_truthy
+      expect(store.name).to be_truthy
+      expect(store.city).to be_truthy
+      expect(store.distance).to be_truthy
+      expect(store.phone).to be_truthy
+      expect(store.type).to be_truthy
     end
   end
 end
